7e0763fc72ee04a6ad6cea21eb5e731e9120e079
Alex Clontz <alexclontz@gmail.com>
2018-06-12 20:58:04 -0700

    hw9 work

--- App.js
+++ App.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
-import { Alert, Button, Text, ToolbarAndroid, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class HelloWorldApp extends Component {
+class Home extends Component {
+    static navigationOptions = {
+        title: "Enshaedn React"
+    };
+    render(){
+        const { navigate } = this.props.navigation;
+        return (
+            <View>
+                <Button
+                    onPress={ () => navigate('Cams')}
+                    title="Traffic Cameras"
+                />
+            </View>
+        )
+    }
+}
+
+const AppNav = StackNavigator({
+    Home: { screen: Home },
+    Cams: { screen: TrafficCams},
+    }, {
+        navigationOptions: {
+            headerStyle: {
+                backgroundColor: '#ef7015'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        }
+});
+
+export default class App extends Component {
   render() {
-    return (
-    <View>
-      <View>
-        <ToolbarAndroid
-            title="Enshaedn React"
-            titleColor="#0000ff"/>
-      </View>
-      <View>
-        <Text>React Native is awful.</Text>
-      </View>
-      <View>
-        <Button
-  onPress={() => {
-    Alert.alert('You tapped the button!');
-  }}
-  title="Press Me"
-/>
-      </View>
-    </View>
-    );
+    return <AppNav />;
   }
+}
+
+class TrafficCams extends Component {
+    render() {
+        return (
+            <View>
+            <Text>Screen two!</Text>
+            </View>
+        );
+    }
 }
